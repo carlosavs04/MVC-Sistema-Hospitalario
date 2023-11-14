d43aa8fe54d28c6033c407e11dfc6c67867c18a6
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-11-14 17:55:53 -0600

    Initial commit

---
+++ database/migrations/2023_11_14_094050_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('last_name', 60);
+            $table->string('email', 100)->unique();
+            $table->string('phone_number', 10);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('role_id')->default(1);
+            $table->unsignedBigInteger('insurance_id')->nullable();
+            $table->string('insurance_plan', 60)->nullable();
+            $table->unsignedBigInteger('specialty_id')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
