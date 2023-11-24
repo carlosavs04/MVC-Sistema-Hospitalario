18f34a2e5de304ffa7370ac5efef3728c70e6cc6
wxzzxrdresurrection <magicwizz12@gmail.com>
2023-11-23 20:36:35 -0600

    fixed img route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css','resources/js/app.js'])
-    @vite('resources/js/carrusel.css')
+    @vite('resources/css/carrusel.css')
     <title>Registro</title>
 </head>
 
@@ -84,7 +84,7 @@
         </div>
     </nav>
     <!-- Carrusel -->
-    <img src="../img/" alt="">
+    <img src="{{ asset('img/ncb.jpg') }}" alt="Logo">
 </body>
 
 </html>
