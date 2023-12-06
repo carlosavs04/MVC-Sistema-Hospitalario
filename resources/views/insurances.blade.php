2ae9e404ecccd204c2be414c4225b0010699d898
wxzzxrdresurrection <magicwizz12@gmail.com>
2023-12-06 16:36:23 -0600

    validate modal

--- resources/views/insurances.blade.php
+++ resources/views/insurances.blade.php
@@ -96,7 +96,7 @@
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="name" name="name">
+                            <input required type="text" class="form-control" id="name" name="name">
                         </div>
                     </div>
                 <div class="modal-footer">
